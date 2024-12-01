c489dacc1f9aaaf142ebceef2538facc41eb6e73
Bitar Youssef <bitardev@gmail.com>
2024-12-01 10:58:33 +0100

    Update framer motion

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  distDir: "out",
   output: process.env.NODE_ENV === "production" ? "export" : undefined,
   basePath: process.env.NODE_ENV === "production" ? "/develop-better-website" : "",
 };
